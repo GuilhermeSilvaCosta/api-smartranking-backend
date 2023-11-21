60bbc08bf5eb2b362df5703ede339350a073f553
Guilherme Costa <guilhermecosta@MacBook-Pro-de-Guilherme.local>
2023-11-21 18:16:58 -0300

    Create module players

---
+++ src/players/interfaces/player.interface.ts
@@ -0,0 +1,9 @@
+export interface Player {
+  readonly _id: string;
+  readonly phone: string;
+  readonly email: string;
+  name: string;
+  ranking: string;
+  positionRanking: number;
+  avatar: string;
+}
